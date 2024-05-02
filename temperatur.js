070b84566ced426c852c661c53a937379a171359
Kevin Böhning <ke.boehning@icloud.com>
2024-05-02 17:42:46 +0200

    Dateien einfügen

---
+++ temperatur.js
@@ -0,0 +1,8 @@
+// Nehmen Sie die Temperatur in Celsius
+var celsius = 25;
+
+// Berechnen Sie die Temperatur in Fahrenheit
+var fahrenheit = celsius * 9/5 + 32;
+
+// Zeigen Sie das Ergebnis in der Konsole an
+console.log("Temperatur in Fahrenheit: " + fahrenheit);
